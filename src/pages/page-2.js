3eb0631f3f1d0cac5e2bb577d76eb92fbbc86ef6
ndiagamactar <ndiagamactarfall@gmail.com>
2018-04-03 05:14:55 +0000

    change to page 2

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -3,7 +3,8 @@ import Link from 'gatsby-link'
 
 const SecondPage = () => (
   <div>
-    <h1>Hi from the second page</h1>
+    <h1>Cecie  n'est qu'un test </h1>
+	<h1>Il est fait avec Gatsby  </h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
   </div>
